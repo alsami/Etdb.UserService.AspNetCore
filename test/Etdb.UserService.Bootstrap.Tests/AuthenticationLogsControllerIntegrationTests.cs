46d87498faceb0a0d01188aea6243d877e848a3b
alsami <al.khatib.sami1990@gmail.com>
2019-08-08 16:07:51 +0200

    chore(tests): add AuthenticationLogsController integration-tests

---
+++ test/Etdb.UserService.Bootstrap.Tests/AuthenticationLogsControllerIntegrationTests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Etdb.UserService.Bootstrap.Tests.Common;
+using Etdb.UserService.Bootstrap.Tests.Fixtures;
+using Etdb.UserService.Domain.Enums;
+using Etdb.UserService.Presentation.Authentication;
+using Etdb.UserService.Presentation.Users;
+using IdentityModel.Client;
+using Xunit;
+
+namespace Etdb.UserService.Bootstrap.Tests
+{
+    public class AuthenticationLogsControllerIntegrationTests : ControllerIntegrationTests
+    {
+        public AuthenticationLogsControllerIntegrationTests(ConfigurationFixture configurationFixture,
+            TestServerFixture testServerFixture) : base(configurationFixture, testServerFixture)
+        {
+        }
+
+        [Fact]
+        public async Task AuthenticationLogsController_LoadAsync_Single_Authentication_Returns_One_AuthenticationLog()
+        {
+            var client = this.TestServerFixture.ApiServer.CreateClient();
+
+            var (_, accessToken, identityUser) = await this.RegisterAuthenticateAndLoadIdentityUserAsync(client);
+
+            client.SetBearerToken(accessToken.AccessToken);
+
+            var authenticationLogsLoadResponse =
+                await client.GetAsync($"api/v1/users/{identityUser.Id}/authentication-logs");
+
+            Assert.True(authenticationLogsLoadResponse.IsSuccessStatusCode,
+                await authenticationLogsLoadResponse.Content.ReadAsStringAsync());
+
+            var authenticationLogs = await authenticationLogsLoadResponse.Content.ReadAsAsync<AuthenticationLogDto[]>();
+
+            Assert.Single(authenticationLogs);
+            Assert.Equal(authenticationLogs.First().AuthenticationLogType, AuthenticationLogType.Succeeded.ToString(),
+                StringComparer.InvariantCultureIgnoreCase);
+        }
+
+
+        private async Task<(UserRegisterDto, TokenResponse, IdentityUserDto)>
+            RegisterAuthenticateAndLoadIdentityUserAsync(HttpClient client)
+        {
+            var registerDto = await RegisterAssertedAsync(client);
+
+            var accessTokenDto = await this.GetTokenAsync(registerDto);
+
+            var identityUserDto = await LoadIdentityUserAssertedAsync(accessTokenDto.AccessToken, client);
+
+            return (registerDto, accessTokenDto, identityUserDto);
+        }
+    }
+}
