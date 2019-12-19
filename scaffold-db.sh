8e31c1a2d7b0e473476d4fe0d1c75f9afe0ecab9
alsami <al.khatib.sami1990@gmail.com>
2019-12-19 17:32:06 +0100

    fix: make scaffolding-script work again

--- scaffold-db.sh
+++ scaffold-db.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 export scaffold_path=src/Etdb.UserService.Scaffolder/Etdb.UserService.Scaffolder.csproj
-export dll_path=src/Etdb.UserService.Scaffolder/bin/Debug/netcoreapp3.0/Etdb.UserService.Scaffolder.dll
+export dll_path=src/Etdb.UserService.Scaffolder/bin/Debug/netcoreapp3.1/Etdb.UserService.Scaffolder.dll
 echo running context scaffold
 dotnet build ${scaffold_path}
 dotnet ${dll_path}
